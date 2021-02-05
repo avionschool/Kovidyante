53d65e1a828398ae84d48fe5e16e99751e72a104
lfigueras <lovelyfigueras@gmail.com>
2021-02-06 07:17:24 +0800

    Finished attendance scanning code.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,33 @@
 class StudentsController < ApplicationController
-    
+    def index
+        require "uri"
+    require "net/http"
+
+    url = URI("https://rest-api-lj.herokuapp.com/post")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Content-Type"] = "application/json"
+   
+    response = https.request(request)
+    puts response.read_body
+    result = JSON.parse(response.read_body)
+    puts result[0]['title']
+    student_id = result[0]['title']
+    api_id = result[0]['_id']
+
+url = URI("https://rest-api-lj.herokuapp.com/post/#{api_id}")
+
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+request = Net::HTTP::Delete.new(url)
+request["Content-Type"] = "application/json"
+
+response = https.request(request)
+puts response.read_body
+puts 'deleted'
+    end
 end
