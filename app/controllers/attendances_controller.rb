f882699ca03c438ea5a41eea54e592e9e9bc9e2d
lfigueras <lovelyfigueras@gmail.com>
2021-02-06 23:11:31 +0800

    Fixed Attendance save to db.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,55 @@
 class AttendancesController < ApplicationController
 
     before_action :authenticate_user!
+  
+    def index
+
+      require "uri"
+      require "net/http"
+  
+      url = URI("https://rest-api-lj.herokuapp.com/post")
+  
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
+  
+      request = Net::HTTP::Get.new(url)
+      request["Content-Type"] = "application/json"
+     
+      response = https.request(request)
+      puts response.read_body
+      result = JSON.parse(response.read_body)
+      
+      if result === []
+          puts "no result"
+      else
     
-    def new
+          puts result[0]['title']
+          student_id = result[0]['title']
+          api_id = result[0]['_id']
+    
+      url = URI("https://rest-api-lj.herokuapp.com/post/#{api_id}")
+      
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
+      
+      request = Net::HTTP::Delete.new(url)
+      request["Content-Type"] = "application/json"
+      
+      response = https.request(request)
+      puts response.read_body
+      puts 'deleted'
+          # byebug
+     
+      end
 
+      present = Attendance.create(status: true, student_id:  student_id.to_i, student_name: current_user.name)
+      
+      end
 
-    end
+      private
+
+      def attendance_params
+        params.require(:attendance).permit(:status, :student_id, :student_name)
+      end
+  
   end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,45 +1,48 @@
 class StudentsController < ApplicationController
-    def index
+#     def index
 
-    require "uri"
-    require "net/http"
+#     require "uri"
+#     require "net/http"
 
-    url = URI("https://rest-api-lj.herokuapp.com/post")
+#     url = URI("https://rest-api-lj.herokuapp.com/post")
 
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
+#     https = Net::HTTP.new(url.host, url.port)
+#     https.use_ssl = true
 
-    request = Net::HTTP::Get.new(url)
-    request["Content-Type"] = "application/json"
+#     request = Net::HTTP::Get.new(url)
+#     request["Content-Type"] = "application/json"
    
-    response = https.request(request)
-    puts response.read_body
-    result = JSON.parse(response.read_body)
+#     response = https.request(request)
+#     puts response.read_body
+#     result = JSON.parse(response.read_body)
     
-    if result === []
-        puts "no result"
-    else
+#     if result === []
+#         puts "no result"
+#     else
   
-        puts result[0]['title']
-        student_id = result[0]['title']
-        api_id = result[0]['_id']
-    
-    url = URI("https://rest-api-lj.herokuapp.com/post/#{api_id}")
-    
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-    
-    request = Net::HTTP::Delete.new(url)
-    request["Content-Type"] = "application/json"
-    
-    response = https.request(request)
-    puts response.read_body
-    puts 'deleted'
+#         puts result[0]['title']
+#         student_id = result[0]['title']
+#         api_id = result[0]['_id']
 
+#     url = URI("https://rest-api-lj.herokuapp.com/post/#{api_id}")
+    
+#     https = Net::HTTP.new(url.host, url.port)
+#     https.use_ssl = true
+    
+#     request = Net::HTTP::Delete.new(url)
+#     request["Content-Type"] = "application/json"
+    
+#     response = https.request(request)
+#     puts response.read_body
+#     puts 'deleted'
+#         # byebug
    
-    end
+#     end
   
-  
+#   private
+#   def attendance_params
+#     params.permit(:status, :student_id)
+#   end
  
-    end
+#     end
 end
